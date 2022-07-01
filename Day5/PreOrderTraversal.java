191e25770286458cbeafc95a4b1dc01efdfc2333
Sravanti Chinta <91924223+ShravesChinta@users.noreply.github.com>
2022-06-30 22:39:26 -0600

    Leetcode 144. Binary Tree Preorder Traversal
    Leetcode 144. Binary Tree Preorder Traversal

---
+++ Day5/PreOrderTraversal.java
@@ -0,0 +1,51 @@
+/**Given the root of a binary tree, return the preorder traversal of its nodes' values.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,null,2,3]
+Output: [1,2,3]
+Example 2:
+
+Input: root = []
+Output: []
+Example 3:
+
+Input: root = [1]
+Output: [1]
+
+***/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> a=new ArrayList<>();
+        preorder(root, a);  
+        return a;
+    }
+    public static void preorder(TreeNode root, List<Integer> a)
+    {
+        if(root==null)
+            return;
+        
+        a.add(root.val);
+        preorder(root.left,a);
+        preorder(root.right,a);
+    }
+}
