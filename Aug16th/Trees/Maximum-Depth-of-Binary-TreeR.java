a05654433e71c4c18da12ac9b339354e6c63c600
Sravanti Chinta <91924223+ShravesChinta@users.noreply.github.com>
2022-08-16 11:26:24 -0600

    104. Maximum Depth of Binary Tree LeetCode
    104. Maximum Depth of Binary Tree LeetCode

---
+++ Aug16th/Trees/Maximum-Depth-of-Binary-TreeR.java
@@ -0,0 +1,59 @@
+/***
+Given a binary tree, find its minimum depth.
+
+The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+
+Note: A leaf is a node with no children.
+
+ 
+
+Example 1:
+
+
+Input: root = [3,9,20,null,null,15,7]
+Output: 2
+Example 2:
+
+Input: root = [2,null,3,null,4,null,5,null,6]
+Output: 5
+***/
+
+class Solution {
+    public int maxDepth(TreeNode root) {
+    return maxDepthB(root);
+        
+    }
+    
+    public static int maxDepthB(TreeNode root)
+    {
+        Queue<TreeNode> q=new LinkedList<>();
+        int depth=0, l=0;
+        
+        if(root==null)
+        return depth;
+        
+        q.add(root);
+        while(q.size()>0)
+        {
+            l=q.size();
+            for(int i=0;i<l;i++)
+            {
+              TreeNode ele=q.remove();
+              if(ele.left!=null)
+              {
+                  q.add(ele.left);
+              }
+                
+                if(ele.right!=null)
+                {
+                    q.add(ele.right);
+                }
+            }
+            depth++;
+        }
+        
+        return depth;
+    }
+    
+    
+}
