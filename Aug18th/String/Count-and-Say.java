5f1ce17dae61cb42227540002cb0a7e2c626ad51
Sravanti Chinta <91924223+ShravesChinta@users.noreply.github.com>
2022-08-18 22:20:00 -0600

    38. Count and Say LeetCode
    38. Count and Say LeetCode

---
+++ Aug18th/String/Count-and-Say.java
@@ -0,0 +1,58 @@
+/***
+The count-and-say sequence is a sequence of digit strings defined by the recursive formula:
+
+countAndSay(1) = "1"
+countAndSay(n) is the way you would "say" the digit string from countAndSay(n-1), which is then converted into a different digit string.
+To determine how you "say" a digit string, split it into the minimal number of substrings such that each substring contains exactly one unique digit. Then for each substring, say the number of digits, then say the digit. Finally, concatenate every said digit.
+
+For example, the saying and conversion for digit string "3322251":
+
+
+Given a positive integer n, return the nth term of the count-and-say sequence.
+
+ 
+
+Example 1:
+
+Input: n = 1
+Output: "1"
+Explanation: This is the base case.
+Example 2:
+
+Input: n = 4
+Output: "1211"
+Explanation:
+countAndSay(1) = "1"
+countAndSay(2) = say "1" = one 1 = "11"
+countAndSay(3) = say "11" = two 1's = "21"
+countAndSay(4) = say "21" = one 2 + one 1 = "12" + "11" = "1211"
+
+**/
+
+class Solution {
+    public String countAndSay(int n) {
+       if(n<=0)
+           return "";
+        String res="1";
+        while(n>1)
+        { 
+           StringBuilder curr=new StringBuilder();
+           for(int i=0;i<res.length();i++)
+           {
+               int count=1;
+               while(i+1<res.length() && res.charAt(i)==res.charAt(i+1))
+               {
+                   count++;
+                   i++;
+               }
+               curr.append(count).append(res.charAt(i));
+           }
+           res=curr.toString();
+            //System.out.println(res);
+           --n;
+        }
+        
+        return res;
+           
+    }
+}
