51090192d18325a06d8724f3d084cb1622661f3c
Sravanti Chinta <91924223+ShravesChinta@users.noreply.github.com>
2022-08-23 23:40:51 -0600

    LeetCode 73. Set Matrix Zeroes
    LeetCode 73. Set Matrix Zeroes

---
+++ Aug22/Arrays/Search-in-a-2D-Matrix-Efficient.java
@@ -0,0 +1,72 @@
+// this solution is much better than the previously solved one.. follow this one only
+
+
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        if(matrix==null || matrix.length==0)
+            return;
+    
+        int row=matrix.length;
+        int column=matrix[0].length;
+        
+        boolean firstRowZero=false;
+        boolean firstColumnZero=false;
+        
+        for(int i=0;i<row;i++)
+        {
+            if(matrix[i][0]==0)
+            {
+                firstRowZero=true;
+                break;
+            }
+        }
+        
+        for(int i=0;i<column;i++)
+        {
+            if(matrix[0][i]==0)
+            {
+                firstColumnZero=true;
+                break;
+            }
+        }
+        
+        for(int i=1;i<row;i++)
+        {
+            for(int j=1;j<column;j++)
+            {
+                if(matrix[i][j]==0)
+                {
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+                }
+            }
+        }
+        
+        for(int i=1;i<row;i++)
+        {
+            for(int j=1;j<column;j++)
+            {
+                if(matrix[i][0]==0 || matrix[0][j] ==0)
+                    matrix[i][j]=0;
+            }
+        }
+        
+        if(firstRowZero)
+        {
+            for(int i=0;i<row;i++)
+            {
+                matrix[i][0]=0;
+            }
+        }
+        
+        if(firstColumnZero)
+        {
+            for(int i=0;i<column;i++)
+            {
+                matrix[0][i]=0;
+            }
+        }
+        
+        }
+    }
+    
