6e9e8c03f1ffe28e10aa9a35d93840e34585cd3c
Sravanti Chinta <91924223+ShravesChinta@users.noreply.github.com>
2022-08-24 01:23:01 -0600

    74. Search a 2D Matrix LeetCode
    74. Search a 2D Matrix LeetCode

---
+++ Aug22/Arrays/Search-in-a-2d-matrix-better.java
@@ -0,0 +1,25 @@
+//this is a much better solution
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix.length==0)
+            return false;
+        
+        int n=matrix.length;
+        int m=matrix[0].length;
+        int r=0,c=m-1;
+        while(r<n && c>=0){
+            if(matrix[r][c]==target){
+                return true;
+            }
+            else if(matrix[r][c]<target){
+                r++;
+            }
+            else {
+                c--;
+            }
+        }
+        return false;
+        
+    }
+}
